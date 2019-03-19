a9029601a96c21415abc76cc89049d9fcdbf962e
Seifeer <ermeson16000@gmail.com>
2019-03-19 17:28:11 -0300

    Primeiros apps

--- app/src/main/java/com/example/myapplication/Tela3Activity.java
+++ app/src/main/java/com/example/myapplication/Tela3Activity.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Tela3Activity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class Tela3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela3);
+    }
+
+    public void exibirToast(View view) {
+        Toast.makeText(this, "Testando o Toast", Toast.LENGTH_SHORT).show();
+    }
+
+    public void aumentarCount(View view) {
+        Toast.makeText(this, "Testando o Count", Toast.LENGTH_SHORT).show();
     }
 }
