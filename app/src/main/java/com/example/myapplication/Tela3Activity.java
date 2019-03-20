be119e87b37321219de0bd37002f08a4b6cd834b
Seifeer <ermeson16000@gmail.com>
2019-03-20 16:38:29 -0300

    Java

--- app/src/main/java/com/example/myapplication/Tela3Activity.java
+++ app/src/main/java/com/example/myapplication/Tela3Activity.java
@@ -3,21 +3,35 @@ package com.example.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Tela3Activity extends AppCompatActivity {
 
+    private TextView txtNumero;
+    private int count;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela3);
+
+        txtNumero=findViewById(R.id.txt_numero);
+        int count = 0;
+        atualizarTela();
     }
 
     public void exibirToast(View view) {
-        Toast.makeText(this, "Testando o Toast", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "O numero na tela é: "+count, Toast.LENGTH_SHORT).show();
     }
 
     public void aumentarCount(View view) {
-        Toast.makeText(this, "Testando o Count", Toast.LENGTH_SHORT).show();
+        count+=1;
+        atualizarTela();
+        //txtNumero.setText("34");
+        //Toast.makeText(this, "Testando o Count", Toast.LENGTH_SHORT).show();
     }
+    public void atualizarTela(){
+        txtNumero.setText(""+count);
+    }
+
 }
